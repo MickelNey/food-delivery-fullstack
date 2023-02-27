a80e82540dae1b32c3ee0f668c6ba0dbca00a550
Mikhail <mihaalmiheev@gmail.com>
2023-02-28 01:19:48 +0300

    updated-single-restaurant-page

--- src/pages/SingleRestaurant/SingleRestaurantPage.tsx
+++ src/pages/SingleRestaurant/SingleRestaurantPage.tsx
@@ -3,6 +3,8 @@ import {useParams} from "react-router-dom";
 import {Page} from "../Page";
 import {useProduct} from "entities/Product";
 import styles from './SingleRestaurant.module.scss'
+import config from "../../shared/config";
+import {ProductWithCart} from "../../features/changeProductInCart";
 
 
 export const SingleRestaurantPage = () => {
@@ -24,37 +26,25 @@ export const SingleRestaurantPage = () => {
         <div className={styles.info}>
           <div className={styles.info_content}>
             <div className={styles.info_logo}>
-              <div>Royal</div>
-              <div className={styles.icon} />
-              <div>Sushi House</div>
+              <div>{data?.title}</div>
+              <div className={styles.icon} >
+
+              </div>
             </div>
 
+            <div className={styles.restaurant_logo}>
+              <img className={styles.restaurant_image} src={`${config.baseUrl}${data?.imageUrl}`}></img>
+            </div>
+
+
             <div className={styles.info_description}>
-              <div className={styles.title}>id {data?.id}</div>
-              <div className={styles.description}>Lorem ipsum dolor sit amet, magna scaevola his ei. Cum te paulo
-                probatus molestiae, eirmod assentior eum ne, et omnis antiopam mel.
-              </div>
+              <div className={styles.title}>Cost {data?.cost} $</div>
               {/*<ProductParams time={'30-40 min'} minSum={'$32'}/>*/}
             </div>
 
+            {data && <ProductWithCart product={data}/>}
+
             <div className={styles.info_map}></div>
-          </div>
-        </div>
-        <div className={styles.content}>
-          <div className={styles.menu}>
-            <div className={styles.menu_title}>
-              <div>Menu</div>
-
-              <div className={styles.menu_layers}>
-                <div className={styles.menu_layer}></div>
-                <div className={styles.menu_layer}></div>
-              </div>
-            </div>
-          </div>
-
-          <div className={styles.reviews}>
-            <div className={styles.content_title}>Reviews</div>
-
           </div>
         </div>
       </div>
